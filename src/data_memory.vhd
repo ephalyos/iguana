7b167590384d6c5dccf9b150da3e3df3a964a83b
C. H. Pelayo <pelayohe94@gmail.com>
2023-06-27 00:47:42 -0600

    Update data_memory.vhd

--- src/data_memory.vhd
+++ src/data_memory.vhd
@@ -16,40 +16,28 @@ end entity;
 
 architecture behavior of data_memory is 
 
-  -- memory definition
   type m1024x8 is array(0 to 2**10 - 1) of std_logic_vector(7 downto 0);
   
-  -- data memory
   signal memory : m1024x8 := (
     
-    -- data
-    "00000000", -- MD[0] = 0
-    "00010011", -- MD[1] = 19
-    "00100011", -- MD[2] = 35
-    "01000011", -- MD[3] = 67
-    
-    -- rest of memory
     others => "00000000"
     
   );
   
 begin
   
-  -- for simulation - clk'event and clk = '0' 
-  -- for implementation - falling_edge(clk)
-  
-  reading : process (clk)
+  process (clk)
   begin
-    if (clk'event and clk = '1') then
+    if ( falling_edge(clk) ) then
       if (re = '1') then
         dout <= memory(to_integer(unsigned(addr)));
       end if;
     end if;
   end process;
   
-  writing : process (clk)
+  process (clk)
   begin
-    if (clk'event and clk = '0') then
+    if ( falling_edge(clk) ) then
       if (we = '1') then
         memory(to_integer(unsigned(addr))) <= din;
       end if;
